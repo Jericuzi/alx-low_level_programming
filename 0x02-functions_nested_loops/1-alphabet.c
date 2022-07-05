de27c8c751551b24e55407e1820ff27505a7b44e
Jericuzi <cjerry557@gmail.com>
2022-07-05 04:03:50 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ * Return: always return 0
+ */
+void print_alphabet(void)
+{
+char j = 'a';
+while (j <= 'z')
+{
+_putchar(j);
+j++;
+}
+_putchar('\n');
+}
